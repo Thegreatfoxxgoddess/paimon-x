1fd8dded77efee804c743268bc578ded717c6794
LostB053 <72547660+lostb053@users.noreply.github.com>
2021-01-15 09:19:03 +0530

    Update year_progress.py

--- userge/plugins/fun/year_progress.py
+++ userge/plugins/fun/year_progress.py
@@ -11,31 +11,9 @@ async def progresss(message):
     day = int(x.strftime("%j"))
     total_days = 365 if x.year % 4 != 0 else 366  # Haha Yes Finally
     percent = math.trunc(day / total_days * 100)
-    num = round(percent / 5)
+    await message.edit(make_bar(percent) + f" {percent}%")
 
-    progress = [
-        "░░░░░░░░░░░░░░░░░░░░",
-        "▓░░░░░░░░░░░░░░░░░░░",
-        "▓▓░░░░░░░░░░░░░░░░░░",
-        "▓▓▓░░░░░░░░░░░░░░░░░",
-        "▓▓▓▓░░░░░░░░░░░░░░░░",
-        "▓▓▓▓▓░░░░░░░░░░░░░░░",
-        "▓▓▓▓▓▓░░░░░░░░░░░░░░",
-        "▓▓▓▓▓▓▓░░░░░░░░░░░░░",
-        "▓▓▓▓▓▓▓▓░░░░░░░░░░░░",
-        "▓▓▓▓▓▓▓▓▓░░░░░░░░░░░",
-        "▓▓▓▓▓▓▓▓▓▓░░░░░░░░░░",
-        "▓▓▓▓▓▓▓▓▓▓▓░░░░░░░░░",
-        "▓▓▓▓▓▓▓▓▓▓▓▓░░░░░░░░",
-        "▓▓▓▓▓▓▓▓▓▓▓▓▓░░░░░░░",
-        "▓▓▓▓▓▓▓▓▓▓▓▓▓▓░░░░░░",
-        "▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓░░░░░",
-        "▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓░░░░",
-        "▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓░░░",
-        "▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓░░",
-        "▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓░",
-        "▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓",
-    ]
 
-    message_out = f"<b>Year Progress</b>\n<code>{progress[num]} {percent}%</code>"
-    await message.edit(message_out)
+def make_bar(per):
+    done = round(per / 5)
+    return "▓" * done + "░" * (20 - done)
