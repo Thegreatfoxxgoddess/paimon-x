787f61974f34b7739c0ca991b9a795f95f6802b5
Alícia <61088084+Thegreatfoxxgoddess@users.noreply.github.com>
2021-03-27 17:36:19 +0530

    Update repo.py

--- userge/plugins/tools/repo.py
+++ userge/plugins/tools/repo.py
@@ -12,5 +12,5 @@ from userge import Config, Message, userge
 @userge.on_cmd("repo", about={"header": "get repo link and details"})
 async def see_repo(message: Message):
     """see repo"""
-    output = f"• **repo** : [USERGE-X]({Config.UPSTREAM_REPO})"
+    output = f"• **repo** : [paimon]({Config.UPSTREAM_REPO})"
     await message.edit(output)
