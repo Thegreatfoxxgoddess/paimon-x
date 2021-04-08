5f0761bcb0c240638f426bb6cab467a7afbc30f8
code-rgb <62891774+code-rgb@users.noreply.github.com>
2021-04-08 15:11:35 +0000

    auto: black code format

--- userge/plugins/help.py
+++ userge/plugins/help.py
@@ -846,7 +846,9 @@ if userge.has_bot:
                     )
                     return
                 else:
-                    await inline_query.answer('No results found!!!', cache_time=1, show_alert=True)
+                    await inline_query.answer(
+                        "No results found!!!", cache_time=1, show_alert=True
+                    )
 
             if str_y[0] == "spoiler":
                 if not os.path.exists(f"{Config.CACHE_PATH}/spoiler_db.json"):
