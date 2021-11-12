0eff67751bd8d854c265e24881ef8deff665f693
Frida <92047243+fridaellora@users.noreply.github.com>
2021-11-12 17:41:41 +0530

    Update flacbot.py

--- userge/plugins/jutsu/flacbot.py
+++ userge/plugins/jutsu/flacbot.py
@@ -47,7 +47,6 @@ async def flac_bot(message: Message):
         await userge.copy_message(
             message.chat.id, bot_, music_.message_id, reply_to_message_id=reply_to
         )
-     if not results.results:
         await message.delete()
     except BaseException as e:
         await message.edit(
