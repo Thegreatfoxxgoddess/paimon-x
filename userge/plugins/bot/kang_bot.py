904ac54be9f3e314d4c05b4c5b06cf377cc2e803
Alícia <61088084+Thegreatfoxxgoddess@users.noreply.github.com>
2022-02-03 20:17:38 +0530

    Update kang_bot.py

--- userge/plugins/bot/kang_bot.py
+++ userge/plugins/bot/kang_bot.py
@@ -239,13 +239,4 @@ def resize_photo(photo: str) -> io.BytesIO:
 
 KANGING_STR = (
     "Using Witchery to kang this sticker...",
-    "Plagiarising hehe...",
-    "Inviting this sticker over to my pack...",
-    "Kanging this sticker...",
-    "Hey that's a nice sticker!\nMind if I kang?!..",
-    "hehe me stel ur stikér\nhehe.",
-    "Ay look over there (☉｡☉)!→\nWhile I kang this...",
-    "Roses are red violets are blue, kanging this sticker so my pacc looks cool",
-    "Imprisoning this sticker...",
-    "Mr.Steal Your Sticker is stealing this sticker... ",
-)
+  )
