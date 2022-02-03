591cf79da97e07914fbb77c89f29f5d3077687b2
Alícia <61088084+Thegreatfoxxgoddess@users.noreply.github.com>
2022-02-03 20:06:46 +0530

    Update kang_bot.py

--- userge/plugins/bot/kang_bot.py
+++ userge/plugins/bot/kang_bot.py
@@ -111,8 +111,7 @@ async def kang_on_send(_, message: Message):
             exist = False
             try:
                 exist = await userge.send(
-                    GetStickerSet(
-                        stickerset=InputStickerSetShortName(short_name=packname)
+                    GetStickerSet(stickerset=InputStickerSetShortName(short_name=packname), hash=0)
                     )
                 )
             except StickersetInvalid:
